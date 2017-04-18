1c1dd0519ef9ebfcec3e43011c7795fed98a1fe5
Alexander Kuzmenko <bafojo@gmail.com>
2017-04-18 21:30:30 +0300

    add [0]

--- vk-frame/script.js
+++ vk-frame/script.js
@@ -1,6 +1,7 @@
 $(function() {
     VK.init(function() {
         VK.api("users.get", {"user_ids": "32931152", "fields": "photo_200,city"}, function (data) {
+            console.log(data);
             $('#name').text(data.response[0].first_name + ' ' + data.response[0].last_name + ' (' + data.response[0].id + ')');
             $('#city').text(data.response[0].city.title);
             $('#photo').attr("src",data.response[0].photo_200);
