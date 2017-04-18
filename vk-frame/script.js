6ed0a91104b6ff42a13f7cf792d1aa45610dac29
Alexander Kuzmenko <bafojo@gmail.com>
2017-04-18 21:28:32 +0300

    add [0]

--- vk-frame/script.js
+++ vk-frame/script.js
@@ -1,9 +1,9 @@
 $(function() {
     VK.init(function() {
         VK.api("users.get", {"user_ids": "32931152", "fields": "photo_200,city"}, function (data) {
-            $('#name').text(data.response.first_name + ' ' + data.response.last_name + ' (' + data.response.id + ')');
-            $('#city').text(data.response.city.title);
-            $('#photo').attr("src",data.response.photo_200);
+            $('#name').text(data.response[0].first_name + ' ' + data.response[0].last_name + ' (' + data.response[0].id + ')');
+            $('#city').text(data.response[0].city.title);
+            $('#photo').attr("src",data.response[0].photo_200);
         });
     }, function() {
         // API initialization failed
