a645fd9d74c5c8aee168997176e89b1eda33fe3b
Alexander Kuzmenko <bafojo@gmail.com>
2017-04-22 21:19:22 +0300

    add method photos.getAll 1 iteration

--- vk-frame/script.js
+++ vk-frame/script.js
@@ -3,8 +3,7 @@ $(function() {
         var myId = 32931152;
 
         var photos = [];
-        var condition = true;
-        for (var offset = 0; condition; offset += 200) {
+        for (var offset = 0; offset < 1000; offset += 200) {
             VK.api("photos.getAll", {"owner_id": myId, "offset":offset,"extended": 1}, function (data) {
                     for (var i = 0, max = data.response.items.length; i < max; i++) {
                         photos.push({
@@ -13,14 +12,14 @@ $(function() {
                             "likes": data.response.likes[i].count
                         });
                     }
-                    if (true || max < 200) { //todo убрать или
-                        condition = false;
+                    if (max < 200) {
                         new Vue ({
                             el: '#app',
                             data: {
                                 items: photos
                             }
                         });
+                        break;
                     }
                 }
             )}
