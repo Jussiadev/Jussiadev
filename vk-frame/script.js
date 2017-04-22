708d841a944f7abf981c54b04bc0e2e8a7e39584
Alexander Kuzmenko <bafojo@gmail.com>
2017-04-22 22:27:05 +0300

    add localozed date

--- vk-frame/script.js
+++ vk-frame/script.js
@@ -1,14 +1,17 @@
 $(function() {
     VK.init(function() {
-        var myId = 32931152;
+        var myId = 32931152,
+        ruMonth = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября',
+            'ноября', 'декабря'];
 
         var photos = [];
        // for (var offset = 0; offset < 1000; offset += 200) {
             VK.api("photos.getAll", {"owner_id": myId, "offset":0,"extended": 1}, function (data) { console.log(data);
                     for (var i = 0, max = data.response.items.length; i < max; i++) {
+                        var d = new Date(data.response.items[i].date*1000);
                         photos.push({
                             "image": data.response.items[i].photo_130,
-                            "date": data.response.items[i].date,
+                            "date": d.getDate() + ' '+ ruMonth[d.getMonth()] + ' ' + d.getFullYear(),
                             "likes": data.response.items[i].likes.count
                         });
                     }
