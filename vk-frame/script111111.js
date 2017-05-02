659f2af5bfd122b85fb9b82362592777f764ecf1
Alexander Kuzmenko <bafojo@gmail.com>
2017-05-02 21:44:55 +0300

    add friend

--- vk-frame/script111111.js
+++ vk-frame/script111111.js
@@ -51,8 +51,7 @@ function getFriends(user_ids) {
             var users = [];
             for (var i = 0, max = data.response.length; i < max; i++) {
                 users.push({
-                    "first_name": data.response[i].first_name,
-                    "second_name": data.response[i].last_name,
+                    "name": data.response[i].first_name + data.response[i].last_name,
                     "ava": data.response[i].photo_100
                 });
             }
