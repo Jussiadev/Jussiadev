2dc51911363bdc491b3cadaa128b13f5cd9b7048
Alexander Kuzmenko <bafojo@gmail.com>
2017-05-07 21:57:36 +0300

    uploadPhotoToWall()

--- vk-frame/script111111.js
+++ vk-frame/script111111.js
@@ -88,9 +88,85 @@ function getMyId() {
     )
 }
 
+
+/**
+ * Постит фото и подпись на стену вк
+ * 1. сформировать картинку на основе шаблона в canvas
+ * 2. Получить картинку из canvas
+ * 3. Получить адрес, куда отправлять картинку (сервер)
+ * 4. Отправить файл по полученному адресу
+ * 5. сохранить фотографию, вызовите метод photos.saveWallPhoto с параметрами server, photo, hash
+ * 6. опубликовав запись с помощью метода wall.post
+ * https://vk.com/dev/upload_files?f=2.%20Загрузка%20фотографий%20на%20стену
+ * @param id
+ */
+
 function uploadPhotoToWall(id) {
     VK.api("photos.getWallUploadServer", {"group_id": id}, function (data){
-            console.log(data);
-        }
-    )
+                canvas = document.getElementById("drawingCanvas");
+                context = canvas.getContext("2d");
+
+                var img1 = new Image();
+// Привязываем функцию к событию onload
+// Это указывает браузеру, что делать, когда изображение загружено
+                img1.onload = function() {
+                    context.drawImage(img1, 10,10,140,170);
+                };
+// Загружаем файл изображения
+                img1.src = app.photos.first;
+
+                var img2 = new Image();
+// Привязываем функцию к событию onload
+// Это указывает браузеру, что делать, когда изображение загружено
+                img2.onload = function() {
+                    context.drawImage(img2,200,10,140,170);
+                };
+// Загружаем файл изображения
+                img2.src = app.photos.second;
+// save canvas image as data url (png format by default)
+
+        var img3 = new Image();
+// Привязываем функцию к событию onload
+// Это указывает браузеру, что делать, когда изображение загружено
+        img3.onload = function() {
+            context.drawImage(img3, 10,10,140,170);
+        };
+// Загружаем файл изображения
+        img1.src = app.photos.third;
+
+                var dataURL = canvas.toDataURL();
+
+                function dataURItoBlob(dataURI) {
+                    // convert base64/URLEncoded data component to raw binary data held in a string
+                    var byteString;
+                    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                        byteString = atob(dataURI.split(',')[1]);
+                    else
+                        byteString = unescape(dataURI.split(',')[1]);
+                    // separate out the mime component
+                    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+                    // write the bytes of the string to a typed array
+                    var ia = new Uint8Array(byteString.length);
+                    for (var i = 0; i < byteString.length; i++) {
+                        ia[i] = byteString.charCodeAt(i);
+                    }
+                    return new Blob([ia], {type:mimeString});
+                }
+
+                var blob = dataURItoBlob(dataURL);
+                var fd = new FormData();
+                fd.append("photo", blob);
+                console.log(fd);
+
+                $.ajax({
+                    url: data.response.upload_url,
+                    data: fd,
+                    processData: false,
+                    contentType: false,
+                    type: 'POST',
+                    success: function(response){
+                        console.log(response);
+                    }
+                });
+            });
 }
