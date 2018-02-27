a8a8e59d30c0f96e65464b693809ac68669f787f
Jussiadev <jussiadev@gmail.com>
2018-02-27 12:38:50 +0300

    fixed bugs

--- vk-frame/script111111.js
+++ vk-frame/script111111.js
@@ -27,13 +27,11 @@ $(function() {
 
     $('#upload_photo').click(function() {
         uploadPhotoToWall(myId);
-        //probnaya(myId);
     });
 });
 
 function loadPhoto(id) {
     var photos = [];
-    // for (var offset = 0; offset < 1000; offset += 200) {
     VK.api("photos.getAll", {"owner_id": id, "offset":0,"extended": 1}, function (data) {
             for (var i = 0, max = data.response.items.length; i < max; i++) {
                 photos.push({
@@ -41,18 +39,14 @@ function loadPhoto(id) {
                     "likes": data.response.items[i].likes.count
                 });
             }
-            //if (max < 200) {
             photos = photos.sort(function(a, b) {
                 return b.likes - a.likes;
             });
             app.photos.first = photos[0].image;
             app.photos.second = photos[1].image;
             app.photos.third = photos[2].image;
-            //  break;
-            //}
         }
     );
-    //}
 }
 
 function getFriends(user_ids) {
